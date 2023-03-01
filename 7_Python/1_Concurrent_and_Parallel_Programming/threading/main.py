c3498d1aed7b176827560a541a91f8907b43e072
u27e34 <adityakumar.agarwal@avl.com>
2023-03-01 01:06:06 +0100

    6. Finished Queues and Master Scheduler

--- 7_Python/1_Concurrent_and_Parallel_Programming/threading/main.py
+++ 7_Python/1_Concurrent_and_Parallel_Programming/threading/main.py
@@ -1,19 +1,38 @@
 import time
 from workers.WikiWorker import WikiWorker
-from workers.YahooFinanceWorkers import YahooFinancepriceWorker
+from workers.YahooFinanceWorkers import YahooFinancepriceScheduler
+
+from multiprocessing import Queue
 
 def main():
     scraper_start_time = time.time()
+    symbol_queue = Queue()
 
     wikiWorker = WikiWorker()
-    current_workers = []
-    for symbol in wikiWorker.get_sp_500_companies():
-        yahooFinancepriceWorker = YahooFinancepriceWorker(symbol=symbol)
-        current_workers.append(yahooFinancepriceWorker)
+
+    yahoo_finance_price_scheduler_threads = []
+
+    # Number of workers that consumes from the Queue i.e. symbol_queue whenever we put something in the queue
+    num_yahoo_finance_price_scheduler_workers = 4
+    for i in range(num_yahoo_finance_price_scheduler_workers):
+        # Although we initialise our threading worker here, but it would wait in class YahooFinancepriceScheduler()
+        # at line "val = self._input_queue.get()" to get a value which happens in "symbol_queue.put(symbol)"
+        yahooFinancepriceScheduler = YahooFinancepriceScheduler(input_queue=symbol_queue)
+        yahoo_finance_price_scheduler_threads.append(yahooFinancepriceScheduler)
     # end
 
-    for i in range(len(current_workers)):
-        current_workers[i].join()
+    for symbol in wikiWorker.get_sp_500_companies():
+        symbol_queue.put(symbol)
+    # end
+
+    # For every instance of queued threads, we put one "DONE" as we want
+    # every instance of the queued thread to break
+    for i in range(len(yahoo_finance_price_scheduler_threads)):
+        symbol_queue.put("DONE")
+    # end
+
+    for i in range(len(yahoo_finance_price_scheduler_threads)):
+        yahoo_finance_price_scheduler_threads[i].join()
     # end
 
 
--- 7_Python/1_Concurrent_and_Parallel_Programming/threading/workers/YahooFinanceWorkers.py
+++ 7_Python/1_Concurrent_and_Parallel_Programming/threading/workers/YahooFinanceWorkers.py
@@ -5,16 +5,35 @@ from lxml import html
 import time
 import random
 
-class YahooFinancepriceWorker(threading.Thread):
+class YahooFinancepriceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
+        super(YahooFinancepriceScheduler, self).__init__(**kwargs)
+        self._input_queue = input_queue
+        self.start()
+
+    def run(self):
+        while True:
+            # Now, this is a blocking operation, which means it's going to block this or this operation is going
+            # to block until we actually get a value returned.
+            val = self._input_queue.get()
+
+            if val == "DONE":
+                break
+
+            yahooFinancepriceWorker = YahooFinancepriceWorker(symbol=val)
+            price = yahooFinancepriceWorker.get_price()
+            print("Price: ", price)
+            time.sleep(5*random.random())
+
+class YahooFinancepriceWorker():
 
     def __init__(self, symbol, **kwargs):
         super(YahooFinancepriceWorker, self).__init__(**kwargs)
         self._symbol = symbol
         base_url = 'https://finance.yahoo.com/quote/'
         self._url = f'{base_url}{self._symbol}'
-        self.start()
 
-    def run(self):
+    def get_price(self):
         # Every thread sleeps in between 0-20 s randomly
         time.sleep(5*random.random())
         r = requests.get(self._url)
