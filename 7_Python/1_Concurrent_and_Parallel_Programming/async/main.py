d0b2ed24f9a1cd3c95e4ec143fc611ba2554c9a9
u27e34 <adityakumar.agarwal@avl.com>
2023-03-04 17:01:20 +0100

    26. Async Libraries

--- 7_Python/1_Concurrent_and_Parallel_Programming/async/main.py
+++ 7_Python/1_Concurrent_and_Parallel_Programming/async/main.py
@@ -1,10 +1,14 @@
 import asyncio
 import time
-import requests
-import aiohttp
+import requests # does not support async
+import aiohttp # supports async
 
 
 async def get_url_response(url):
+    # 3 control stages: 2 async code line and 1 await and at each line it can give control back to main event loop
+    # to continue with the further execution
+    # which is not possible with sync_text_response.append(requests.get(url).text) in line 29 as the
+    # requests.get(url).text is a blocking event for the main event loop
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.text()
